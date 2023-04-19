43f45c7cca1abe323c20f0fdb6daa2741baa0564
L-cir94 <luca.cirigliano94@hotmail.it>
2023-04-19 16:39:16 +0200

    empy card in movielist

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -14,21 +14,7 @@ export default {
 </script>
 
 <template>
-    <div class="row">
-            <div class="col-2">
-                <div class="movie my-5">
-                    <h3>{{movie.title}}</h3>
-                    <h3>{{movie.original_title}}</h3>
-                    <h3>{{movie.original_language}}</h3>
-                    <h3>{{movie.vote_average}}</h3>
-                </div>
-            </div>
-        <div class="col-2">
-            <div class="card">
-
-            </div>
-        </div>
-    </div>
+    
 </template>
 
 <style lang="scss" scoped>
