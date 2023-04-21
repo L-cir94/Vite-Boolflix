ac74b67891b9c2609ccc52a0cdc5d62b77f336d6
L-cir94 <luca.cirigliano94@hotmail.it>
2023-04-21 10:51:05 +0200

    commit for check tutor

--- src/state.js
+++ src/state.js
@@ -7,19 +7,19 @@ export const state = reactive({
     loading: true,
     results: null,
     pagination: null,
-    movieBaseUrl: 'https://api.themoviedb.org/3',
-    apiKey: 'edd3839ffafac261797d7e156df34aba',
-    resourcePath: '/search/movie',
+    movieBaseUrl:'https://api.themoviedb.org/3/',
+    apiKey:'edd3839ffafac261797d7e156df34aba',
+    resourcePath: 'search/movie',
 
     fetchMovies(URL) {
-        const url = `${this.movieBaseUrl + this.resourcePath}?api_key=${this.apiKey}&query=${this.searchText}".`
+        const url = `${this.movieBaseUrl + this.resourcePath}?api_key=${this.apiKey}&query=${this.searchText}`
         console.log(url);
         axios
             .get(URL)
             .then(response => {
                 console.log(response.data.results);
                 this.results = response.data.results;
-                this.loading = true
+                this.loading = false
             })
             .catch(error => {
                 console.log(error.message)
